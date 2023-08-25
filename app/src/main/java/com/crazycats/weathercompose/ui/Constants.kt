5e72467dc5b0895044449e9cdbe8077f87e2dc4e
EugenePonomarev <129969020+EugenePonomarev@users.noreply.github.com>
2023-08-25 17:55:47 +0300

    Update Constants.kt

--- app/src/main/java/com/crazycats/weathercompose/ui/Constants.kt
+++ app/src/main/java/com/crazycats/weathercompose/ui/Constants.kt
@@ -2,7 +2,8 @@ package com.crazycats.weathercompose.ui
 
 object Constants {
     var BASE_URL: String = "https://api.openweathermap.org/data/2.5/"
-    const val API_KEY = "4e59726f61f8b17b06e8daa7605a24b3"
+    //Key - 4e59726f61f8b17b06e8daa7605a24b3
+    const val API_KEY = ""
     const val PERMISSION_REQUEST_CODE = 123
 
     const val DATE_PATTERN = "dd/MM/yyyy HH:mm"
